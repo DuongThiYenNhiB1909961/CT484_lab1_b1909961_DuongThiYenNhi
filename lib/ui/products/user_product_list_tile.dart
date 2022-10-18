6b2dc1c77713b3a06babf8453baa04ad997250a0
DuongThiYenNhiB1909961 <nhib1808@gmail.com>
2022-10-18 20:58:09 +0700

    Quan ly trang thai voi provider - ProductsManager

--- lib/ui/products/user_product_list_tile.dart
+++ lib/ui/products/user_product_list_tile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
+import '../products/products_manager.dart';
 import '../../models/product.dart';
-
+import 'package:provider/provider.dart';
 class UserProductListTile extends StatelessWidget {
   final Product product;
   const UserProductListTile(
@@ -31,8 +31,18 @@ class UserProductListTile extends StatelessWidget {
   Widget buildDeleteButton(BuildContext context) {
     return IconButton(
       icon: const Icon(Icons.delete),
-      onPressed: () async {
-        print('Delete a product');
+      onPressed: () {
+        context.read<ProductsManager>().deleteProduct(product.id!);
+        ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(
+            const SnackBar(
+              content: Text(
+                'Product deleted',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          );
       },
       color: Theme.of(context).errorColor,
     );
--- lib/ui/products/user_products_screen.dart
+++ lib/ui/products/user_products_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'user_product_list_tile.dart';
 import 'products_manager.dart';
 import '../shared/app_drawer.dart';
+import 'package:provider/provider.dart';
 
 class UserProductsSreen extends StatelessWidget {
   static const routeName = '/user-products';
@@ -28,16 +29,20 @@ class UserProductsSreen extends StatelessWidget {
 }
 
 Widget buildUserProductListView(ProductsManager productsManager) {
-  return ListView.builder(
-    itemCount: productsManager.itemsCount,
-    itemBuilder: (ctx, i) => Column(
-      children: [
-        UserProductListTile(
-          productsManager.items[i],
+  return Consumer<ProductsManager>(
+    builder: (ctx, productsManager, child) {
+      return ListView.builder(
+        itemCount: productsManager.itemsCount,
+        itemBuilder: (ctx, i) => Column(
+          children: [
+            UserProductListTile(
+              productsManager.items[i],
+            ),
+            const Divider(),
+          ],
         ),
-        const Divider(),
-      ],
-    ),
+      );
+    },
   );
 }
 
